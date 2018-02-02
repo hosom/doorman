0eb6774dee8afdde38622b1185f4291ac3f905b5
Stephen Hosom <0xhosom@gmail.com>
2018-02-02 13:32:30 -0500

    wrong kwarg

--- basic-client.py
+++ basic-client.py
@@ -13,14 +13,14 @@ if __name__ == '__main__':
 
     p = ArgumentParser('Submit a new indicator to doorman.')
     p.add_argument('doorman',
-                    description='Service instance for doorman.')
+                    help='Service instance for doorman.')
     p.add_argument('indicator',
-                    description='The indicator that you would like to add.')
+                    help='The indicator that you would like to add.')
     p.add_argument('reason',
-                    description='Why this indicator is being added.')
+                    help='Why this indicator is being added.')
     p.add_argument('-t', '--ttl',
                     default='1h',
-                    description='How long the indicator should live for.')
+                    help='How long the indicator should live for.')
     args = p.parse_args()
 
     indicator=dict(
